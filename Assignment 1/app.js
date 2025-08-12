44db6c888c1df32342c6555cb3868a9c8f0ce6fb
Taha <exchangestock17@gmail.com>
2025-08-12 14:53:01 -0700

    javasciptassigmment

---
+++ Assignment 1/app.js
@@ -0,0 +1,28 @@
+//  Q1. Create a full name message using variables. Combine first and last name into fullName and alert
+
+// Answer
+let fname = "Taha"
+let lname = "Qayyum"
+let result = fname + lname
+console.log(result, "TahaQayyum")
+
+
+//  Q2.  What will be the output of: let a = "5"; let b = 2;
+
+// Answer
+let d = "5"
+let b = 2
+let c = d + b
+console.log(c, "52")
+
+// Explanation: a ki value string me mention hai lekin jab hum isay plus karengay to wo sting ki value ko as a number le kar
+//              le ga or isay sath jor dega to iska answer 52 ayega.
+
+
+// Q3. Guess the output and explain: let a = 10; let b = 5; let c = ++a + b-- + --b;
+
+// Answer
+let a = 10;
+let f = 6;
+let u = ++a + --f
+ console.log(u)
---
+++ Assignment 2/app.js
@@ -0,0 +1,144 @@
+// Q1. Write a program that asks the user for their name using prompt and then displays a greeting in an
+//  alert box.
+
+// Answer
+// var name = prompt ("What is your name?");
+//  alert("Hello" + name)
+
+
+// Q2. Ask the user to enter a number. If the number is greater than 10, display 'Number is big'.
+
+// Answer
+// var number = prompt("Enter a number");
+// if (number >10){
+// alert("Number is big")    
+// }
+
+// Q3. Create a variable total with value 50 and add 25 to it, then display the result in an alert.
+
+// Answer
+// var a = 50
+// var b = 25
+// var c = a + b
+// alert(c)
+
+
+//  Q4. Ask the user to enter their favorite color, then display 'Wow! [color] is my favorite too!'.
+
+// Answer
+// var color = prompt("What is your favorite color?");
+// if (color === "White"){
+// alert("Wow"  + color + "color is my favorite too!")    
+// }
+
+
+// Q5. Ask the user for their age using prompt. If the age is greater than or equal to 18, display 'You are
+// eligible to vote'.
+
+// Answer
+// var age = prompt("What is your age?")
+// if  (age >=18){
+//     alert("You are eligible to vote")
+// }
+
+
+//  Q6. Ask the user to enter two numbers and display their sum in an alert.
+
+// Answer
+// var a = Number(prompt("Enter first number"));
+// var b = Number(prompt("Enter second number"));
+// alert(a + b)
+
+
+// Q7. Ask the user to enter 'yes' or 'no'. If the answer is 'yes', display 'Great!'.
+
+// Answer
+// var choice = prompt("Enter yes or no");
+// if (choice === "yes"){
+//     alert("Great!")
+// }
+
+// Q8. Ask the user for a password. If the password is '12345', display 'Access granted'.
+
+// Answer
+// var pass = prompt("Type youre password here");
+// if (pass === "12345"){
+//     alert("Access granted")
+// }
+
+// Q9. Ask the user for two numbers. If the first number is equal to the second, display 'Both are equal'.
+
+// Answer
+// var number1 = prompt("Enter first number")
+// var number2 = prompt("Enter second number")
+// if (number1 == number2){
+//     alert("Both are equal numbers")
+// }
+
+
+// Q10. Create a variable named myName and assign your name to it. Display it using alert.
+
+// Answer
+// var myName = "Taha"
+// alert(myName)
+
+
+// Q11. Ask the user for temperature. If temperature is greater than 30, display 'It's hot today'.
+
+// Answer
+// var temperature = prompt("What is the room temperature today?")
+// if (temperature >=30){
+//     alert("It's hot today")
+// }
+
+
+// Q12. Write a program that concatenates 'Hello' and 'World' with a space in between, then displays
+//     'Hello World' in an alert
+
+// Answer
+// var a = "Hello"
+// var b = "World"
+// var c = a + " " + b
+// alert(c)
+
+
+// Q13. Ask the user for a number. If it is divisible by 2, display 'Even number'.
+
+// Answer
+// var num = prompt("Enter any number here")
+// if (num % 2 === 0){
+//     alert("Even number")
+// }
+
+
+// Q14. Ask the user for their city name. If the city is 'Karachi', display 'Welcome to the city of lights'.
+
+// Answer
+// var city = prompt("Where are you from?")
+// if (city === "Karachi"){
+//     alert("Welcome to the city if lights")
+// }
+
+
+// Q15. Ask the user for their marks. If marks are greater than or equal to 50, display 'Pass', otherwise 'Fail'.
+
+// Answer
+// var marks = prompt("Enter your CSS test marks here")
+// if (marks >= 50){
+//     alert("Pass")
+// }
+// else{
+//     alert("Fail")
+// }
+
+
+
+
+
+
+
+
+
+
+
+ 
